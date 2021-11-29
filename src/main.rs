5468fda014188b994b4066f78fbd77ca76eabe64
FilippoRanza <filipporanza@gmail.com>
2021-11-29 17:56:52 +0100

    Add CLI args parser

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
-use fastrand;
-use ndarray;
-use std::cmp::Ordering;
-
 use rayon::prelude::*;
+use serde::Deserialize;
+use std::fs::File;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 type Int = i32;
 type Mat = ndarray::Array2<Int>;
+
+const MAX_TRIALS: usize = 10;
+const TENURE_SCALE: usize = 5;
+
+#[derive(StructOpt)]
+struct Arguments {
+    traffic_file: PathBuf,
+    max_error: f64,
+    ternure_len: Option<Int>,
+    momentum: Option<f64>,
+    max_noise: Option<Int>,
+}
+
+fn usize_below(th: usize) -> usize {
+    fastrand::usize(0..th)
+}
 
 #[derive(Debug)]
 struct TabuList {
@@ -18,13 +34,16 @@ impl TabuList {
     fn new(size: usize, tenure: Int) -> Self {
         let col_tabu = (0..size).map(|_| 0).collect();
         let row_tabu = (0..size).map(|_| 0).collect();
-        Self { col_tabu, row_tabu, tenure }
+        Self {
+            col_tabu,
+            row_tabu,
+            tenure,
+        }
     }
 
     fn is_locked(&self, idx: (usize, usize)) -> bool {
         let (i, j) = idx;
-        self.row_tabu[i] > 0 &&
-        self.row_tabu[j] > 0
+        self.row_tabu[i] > 0 && self.row_tabu[j] > 0
     }
 
     fn next_step(&mut self) {
@@ -45,7 +64,7 @@ impl TabuList {
     }
 }
 
-fn get_sum<'a>(mat: &'a Mat) -> impl Iterator<Item = Int> + 'a {
+fn get_sum(mat: &'_ Mat) -> impl Iterator<Item = Int> + '_ {
     mat.rows().into_iter().map(|row| row.sum())
 }
 
@@ -55,55 +74,26 @@ fn get_percentage_error<T: Into<f64>>(a: T, b: T) -> f64 {
     (a - b) / a
 }
 
-fn get_error<'a>(mat: &'a Mat, correct: &'a Vec<Int>) -> impl Iterator<Item = (usize, f64)> + 'a {
+fn get_error<'a>(
+    mat: &'a Mat,
+    correct: &'a [Int],
+) -> impl Iterator<Item = (usize, (f64, Int))> + 'a {
     let row_sum = get_sum(mat);
     correct
-        .into_iter()
+        .iter()
         .zip(row_sum)
-        .map(|(a, b)| get_percentage_error(*a, b))
+        .map(|(a, b)| (get_percentage_error(*a, b), a - b))
         .enumerate()
 }
 
-fn f64_comp(a: &(usize, f64, ErrorType), b: &(usize, f64, ErrorType)) -> Ordering {
-    let a = a.1;
-    let b = b.1;
-    if a < b {
-        Ordering::Less
-    } else if b < a {
-        Ordering::Greater
-    } else {
-        Ordering::Equal
-    }
-}
-
-#[derive(Clone, Copy, Debug)]
-enum ErrorType {
-    Positive,
-    Negative,
-}
-
-impl ErrorType {
-    fn from_error(err: (usize, f64)) -> (usize, f64, Self) {
-        if err.1 < 0.0 {
-            (err.0, err.1.abs(), Self::Negative)
-        } else {
-            (err.0, err.1.abs(), Self::Positive)
-        }
-    }
-}
-
-fn outside_bound(
-    mat: &Mat,
-    traffic_sum: &Vec<Int>,
-    max_val: f64,
-) -> Option<Vec<(usize, ErrorType)>> {
+fn outside_bound(mat: &Mat, traffic_sum: &[Int], max_val: f64) -> Option<Vec<(usize, Int)>> {
     let perc_errors = get_error(mat, traffic_sum);
-    let mut tmp: Vec<(usize, f64, ErrorType)> = perc_errors
-        .map(ErrorType::from_error)
+    let mut tmp: Vec<(usize, f64, Int)> = perc_errors
+        .map(|(i, (e, a))| (i, e.abs(), a))
         .filter(|(_, e, _)| *e > max_val)
         .collect();
-    if tmp.len() > 0 {
-        tmp.sort_by(f64_comp);
+    if !tmp.is_empty() {
+        fastrand::shuffle(&mut tmp);
         Some(tmp.into_iter().map(|(i, _, t)| (i, t)).collect())
     } else {
         None
@@ -138,11 +128,11 @@ impl IndexTrial {
 
 fn find_next_index(
     tabu: &mut TabuList,
-    idx_list: &[(usize, ErrorType)],
+    idx_list: &[(usize, Int)],
     size: usize,
-) -> Option<((usize, usize), ErrorType)> {
+) -> Option<((usize, usize), Int)> {
     for (i, t) in idx_list {
-        let mut trial = IndexTrial::new(20, *i, size);
+        let mut trial = IndexTrial::new(MAX_TRIALS, *i, size);
         while let Some(j) = trial.next_trial() {
             if !tabu.is_locked((*i, j)) {
                 tabu.set_tabu((*i, j));
@@ -153,41 +143,105 @@ fn find_next_index(
     None
 }
 
-fn update_traffic_value(output: &mut Mat, index: (usize, usize), et: ErrorType) {
-    let (i, j) = index;
-    match et {
-        ErrorType::Positive => {
-            output[(i, j)] += 5;
-            output[(j, i)] += 5;
-        }
-        ErrorType::Negative => {
-            if output[(i, j)] > 1 {
-                output[(i, j)] -= 1;
-                output[(j, i)] -= 1;
-            }
-        }
-    };
+fn scale_value(val: Int, count: usize) -> Int {
+    val / (count as Int)
 }
 
-fn rebuild_arc_traffic(traffic_sum: &Vec<Int>, max_err: f64, tenure: Int) -> Mat {
-    let size = traffic_sum.len();
+fn update_traffic_value(output: &mut Mat, index: (usize, usize), abs_err: Int) {
+    let (i, j) = index;
+    let update = scale_value(abs_err, output.nrows());
+    let tmp = output[(i, j)] + update;
+    if tmp > 0 {
+        output[(i, j)] += update;
+        output[(j, i)] += update;
+    } else {
+        output[(i, j)] = 1;
+        output[(j, i)] = 1;
+    }
+}
+
+fn random_index(size: usize) -> (usize, usize) {
+    (usize_below(size), usize_below(size))
+}
+
+fn potential_noise_index(mat: &Mat) -> Option<(usize, usize)> {
+    let size = mat.ncols();
+    for _ in 0..MAX_TRIALS {
+        let idx = random_index(size);
+        if mat[idx] == 1 {
+            return Some(idx);
+        }
+    }
+    None
+}
+
+fn noise_momentum(output: &mut Mat, momentum: f64, max_val: Int) {
+    let p = fastrand::f64();
+    if p < momentum {
+        let val = fastrand::i32(0..max_val);
+        if let Some((i, j)) = potential_noise_index(output) {
+            output[(i, j)] = val;
+            output[(j, i)] = val;
+        }
+    }
+}
+
+fn rebuild_arc_traffic(config: &Config) -> Mat {
+    let size = config.traffic_vector.len();
     let mut output = ndarray::Array2::zeros((size, size));
-    let mut tabu = TabuList::new(size, tenure);
-    while let Some(error) = outside_bound(&output, traffic_sum, max_err) {
+    let mut tabu = TabuList::new(size, config.tenure);
+    while let Some(error) = outside_bound(&output, &config.traffic_vector, config.max_err) {
         if let Some((idx, et)) = find_next_index(&mut tabu, &error, size) {
             update_traffic_value(&mut output, idx, et)
         }
         tabu.next_step();
+        noise_momentum(&mut output, config.momentum, config.max_noise);
     }
     output
 }
 
-fn main() {
-    let vect = vec![
-        1875, 1696, 2259, 2098, 2008, 2775, 440, 598, 820, 19240, 689, 11795, 30270, 27917, 5658,
-        1761, 2259, 1059, 3000, 838, 958, 1772, 3836,
-    ];
-    let mat = rebuild_arc_traffic(&vect, 0.05, 50);
+struct Config {
+    traffic_vector: Vec<Int>,
+    max_err: f64,
+    tenure: Int,
+    momentum: f64,
+    max_noise: Int,
+}
+
+impl Config {
+    fn from_args(args: Arguments) -> Result<Self, Box<dyn std::error::Error>> {
+        let traffic_vector = load_traffic_vector(args.traffic_file)?;
+        let tenure = args
+            .ternure_len
+            .unwrap_or_else(|| get_default_tenure(&traffic_vector));
+        let momentum = args.momentum.unwrap_or(0.0);
+        let max_noise = args.max_noise.unwrap_or(0);
+        Ok(Self {
+            traffic_vector,
+            max_err: args.max_error,
+            tenure,
+            momentum,
+            max_noise,
+        })
+    }
+}
+
+fn get_default_tenure<T>(tv: &[T]) -> Int {
+    let len = tv.len();
+    (len / TENURE_SCALE) as Int
+}
+
+#[derive(Deserialize)]
+struct TrafficVector(Vec<Int>);
+
+fn load_traffic_vector(file_path: PathBuf) -> Result<Vec<Int>, Box<dyn std::error::Error>> {
+    let file = File::open(file_path)?;
+    let traffic_vector: TrafficVector = serde_json::from_reader(file)?;
+    Ok(traffic_vector.0)
+}
+
+fn run_tabu_search(config: Config) {
+    let mat = rebuild_arc_traffic(&config);
 
     for row in mat.rows() {
         for v in row {
@@ -195,7 +249,15 @@ fn main() {
         }
         println!();
     }
-    for val in get_sum(&mat).zip(&vect) {
+    for val in get_sum(&mat).zip(&config.traffic_vector) {
         println!("{:?}", val)
+    }
+}
+fn main() {
+    let args = Arguments::from_args();
+
+    match Config::from_args(args) {
+        Ok(config) => run_tabu_search(config),
+        Err(err) => eprintln!("Error: {:?}", err),
     }
 }
